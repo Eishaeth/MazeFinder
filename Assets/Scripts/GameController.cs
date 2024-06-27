93872e6a63f85e0aeb74f23b2d756383ec00979d
TaroGrape <beatriceboo@hotmail.com>
2024-06-27 15:02:38 +0800

    Added audio
    added game over audio
    
    changed transparent material to slight yellow
    
    updated GameController.cs script for game over music

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,51 +8,136 @@ public class GameController : MonoBehaviour
     public Text timeCounter, FinalTimeText;
     public bool gamePlaying { get; private set; }
 
+    public AudioSource audioSourcesToMuteOne, audioSourcesToMuteTwo, gameOverAudioSource;
+
     private float startTime, elapsedTime;
-    TimeSpan timePlaying;
+    private TimeSpan timePlaying;
 
     private void Start()
     {
-         BeginGame();
+        BeginGame();
     }
 
     private void BeginGame()
     {
         gamePlaying = true;
         startTime = Time.time;
-        gameOverPanel.SetActive(false);
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("gameOverPanel is not assigned in BeginGame");
+        }
     }
 
     private void Update()
     {
-        if(gamePlaying)
+        if (gamePlaying)
         {
             elapsedTime = Time.time - startTime;
             timePlaying = TimeSpan.FromSeconds(elapsedTime);
 
             string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
-            timeCounter.text = timePlayingStr;
+            if (timeCounter != null)
+            {
+                timeCounter.text = timePlayingStr;
+            }
+            else
+            {
+                Debug.LogError("timeCounter is not assigned in Update");
+            }
         }
     }
+
     void SetTextAlpha(Text textComponent, float alphaValue)
     {
-        Color textColor = textComponent.color;
-        textColor.a = alphaValue;
-        textComponent.color = textColor;
+        if (textComponent != null)
+        {
+            Color textColor = textComponent.color;
+            textColor.a = alphaValue;
+            textComponent.color = textColor;
+        }
+        else
+        {
+            Debug.LogError("Text component is not assigned in SetTextAlpha");
+        }
     }
+
     public void EndGame()
     {
         gamePlaying = false;
         Debug.Log("EndGame method called");
         Invoke("ShowGameOverScreen", 1.25f);
         SetTextAlpha(timeCounter, 0f);
+
+        if (audioSourcesToMuteOne != null)
+        {
+            audioSourcesToMuteOne.mute = true;
+        }
+        else
+        {
+            Debug.LogError("audioSourcesToMuteOne is not assigned in EndGame");
+        }
+
+        if (audioSourcesToMuteTwo != null)
+        {
+            audioSourcesToMuteTwo.mute = true;
+        }
+        else
+        {
+            Debug.LogError("audioSourcesToMuteTwo is not assigned in EndGame");
+        }
+
+        if (gameOverAudioSource != null)
+        {
+            Invoke("UnmuteGameOverAudioSource", 1.25f);
+            gameOverAudioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("gameOverAudioSource is not assigned in EndGame");
+        }
     }
+
     private void ShowGameOverScreen()
     {
         Debug.Log("ShowGameOverScreen method called");
-        gameOverPanel.SetActive(true);
-        string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
-        gameOverPanel.transform.Find("FinalTimeText").GetComponent<Text>().text = timePlayingStr;
-        Debug.Log("gameOverPanel should now be active");
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            if (FinalTimeText != null)
+            {
+                FinalTimeText.text = timePlayingStr;
+            }
+            else
+            {
+                Debug.LogError("FinalTimeText is not assigned in ShowGameOverScreen");
+            }
+            Debug.Log("gameOverPanel should now be active");
+        }
+        else
+        {
+            Debug.LogError("gameOverPanel is not assigned in ShowGameOverScreen");
+        }
+    }
+
+    private void UnmuteAudioSource(AudioSource audioSource)
+    {
+        if (audioSource != null)
+        {
+            audioSource.mute = false;
+        }
+        else
+        {
+            Debug.LogError("AudioSource is not assigned in UnmuteAudioSource");
+        }
+    }
+
+    private void UnmuteGameOverAudioSource()
+    {
+        UnmuteAudioSource(gameOverAudioSource);
     }
 }
